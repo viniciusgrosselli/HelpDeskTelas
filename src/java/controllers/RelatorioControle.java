6d1a7780c870d2ddaf34bfe3f26bb1aa0cd2e1db
Vinicius <vinnigrosselli@gmail.com>
2017-06-04 20:48:02 -0300

    relatorios

--- src/java/controllers/RelatorioControle.java
+++ src/java/controllers/RelatorioControle.java
@@ -3,13 +3,7 @@
  * and open the template in the editor.
  */
 package controllers;
-
-import com.lowagie.text.Cell;
-import com.lowagie.text.Row;
 import java.sql.Connection;
-
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.HashMap;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -19,8 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import outro.ConexaoRelatorio;
 
 /**
@@ -31,7 +23,11 @@ import outro.ConexaoRelatorio;
 @SessionScoped
 public class RelatorioControle {
 
-    
+    public String uf;
+    public String equiptipo;
+    public String status;
+    public String nome;
+
 
     public RelatorioControle() {
     }
@@ -63,7 +59,32 @@ public class RelatorioControle {
         }
     }
 
-  
+    // CIDADES POR ESTADOS
+    public void relCidade2() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("ESTADO_UF", uf);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relCidade2.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     //RELATÓRIO ESTADOS
     public void relEstado() {
@@ -117,7 +138,32 @@ public class RelatorioControle {
         }
     }
 
-  
+    // Peças por equipamentos
+    public void relPeca1() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("EQUIPAMENTO_TIPO", equiptipo);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relPeca1.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     // SERVIÇOS
     public void relServico() {
@@ -196,11 +242,195 @@ public class RelatorioControle {
             e.printStackTrace();
         }
     }
+
+    public void relChamado() {
+        try {
+            HashMap parameters = new HashMap();
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relChamado.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void relChamado1() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("STATUS", status);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relChamado1.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void relChamado2() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("NOME", nome);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relChamado2.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void relChamado3() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("NOME", nome);
+            parameters.put("STATUS", status);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relChamado3.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void relChamado4() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("NOME", nome);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relChamado4.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void relChamado5() {
+        try {
+            HashMap parameters = new HashMap();
+            parameters.put("NOME", nome);
+            parameters.put("STATUS", status);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.responseComplete();
+            ServletContext scontext = (ServletContext) facesContext
+                    .getExternalContext().getContext();
+            Connection con = ConexaoRelatorio.getEntityManagerJDBCConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath("WEB-INF/relatorios/relChamado5.jasper"),
+                    parameters, con);
+
+            byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+            HttpServletResponse res = (HttpServletResponse) FacesContext
+                    .getCurrentInstance().getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline;filename=arquivo.pdf"); //Abrir em nova página
+            //res.setHeader("Content-disposition","attachment;filename=arquivo.pdf");// baixar arquivo PDF
+            res.getOutputStream().write(b);
+            res.getCharacterEncoding();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getEquiptipo() {
+        return equiptipo;
+    }
+
+    public void setEquiptipo(String equiptipo) {
+        this.equiptipo = equiptipo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
 }
-
-
- 
-
-
-
-    
